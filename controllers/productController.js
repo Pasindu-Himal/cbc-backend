88d03f47fe3ab1229c7b8827275a2426a4e39458
Pasindu <pasinduhimal996@gmail.com>
2025-04-10 00:54:05 +0530

    initializing the project

---
+++ controllers/productController.js
@@ -0,0 +1,80 @@
+import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
+
+//export async function getProduct(req, res) {
+// Product.find().then(
+//   (data) => {
+//   res.json(data);
+// }).catch(
+//   (err)=>{
+//     res.json({
+//       message : "Failed to get products",
+//       error : err
+//     })
+//   }
+// )
+
+export async function getProduct(req, res) {
+  try {
+    if (isAdmin(req)) {
+      const products = await Product.find();
+      res.json(products);
+    } else {
+      const products = await Product.find({ isAvailable: true });
+      res.json(products);
+    }
+  } catch (err) {
+    res.json({
+      message: "Failed to get products",
+      error: err,
+    });
+  }
+}
+
+export function saveProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "You are not authorized to add a product",
+    });
+    return;
+  }
+
+  //console.log(req.user);
+  //console.log(req.body);
+
+  const product = new Product(req.body);
+
+  product
+    .save()
+    .then(() => {
+      res.json({
+        message: "Product added successfully",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "Failed to add product",
+      });
+    });
+}
+
+export async function deleteProduct(req, res) {
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "You are not authorized to delete a product",
+    });
+    return;
+  }
+
+  try {
+    await Product.deleteOne({ productId: req.params.productId });
+    res.json({
+      message: "Product deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to delete product",
+      error: err,
+    });
+  }
+}
---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import productRouter from "./routes/productRoute.js";
+import userRouter from "./routes/userRoute.js";
+import jwt from "jsonwebtoken";
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  const tokenString = req.header("Authorization");
+  if (tokenString != null) {
+    const token = tokenString.replace("Bearer ", "");
+    //console.log(token);
+
+    jwt.verify(token, "abc@123", (err, decoded) => {
+      if (decoded != null) {
+        req.user = decoded;
+        next();
+        //console.log(decoded);
+      } else {
+        console.log("Invalid token");
+        res.status(403).json({
+          message: "Invalid token",
+        });
+      }
+    });
+  } else {
+    next();
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1234@cluster0.qgvnz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected to the databases");
+  })
+  .catch(() => {
+    console.log("Databases connection failed");
+  });
+
+app.use("/products", productRouter);
+app.use("/users", userRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
